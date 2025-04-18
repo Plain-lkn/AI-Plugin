559d4578d8e30b708557685aa9dd75e1e794a8f4
Sepro <shtmdwns0@gmail.com>
2025-04-18 10:05:35 +0900

    fix: 예외처리 추가

--- app/api/ai/dummy.py
+++ app/api/ai/dummy.py
@@ -8,9 +8,9 @@ router = APIRouter()
 
 @router.post("/dummy")
 async def create_dummy_data(request: HistoryDTO, session: AsyncSession = Depends(get_session)):
-    # try:
-    history_data = await request.history_to_base()
-    await create_history(history_data, session)
-    return {"message": "성공적으로 더미데이터가 생성되었습니다."}
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    try:
+        history_data = await request.history_to_base()
+        await create_history(history_data, session)
+        return {"message": "성공적으로 더미데이터가 생성되었습니다."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
